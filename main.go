2d7e4ec9ed39f9b972003c360ee1dca215fd19ba
AlanLg <alanilateur@gmail.com>
2024-03-24 18:41:03 +0100

    base project structure'

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"dreampicai/handler"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := initEverything(); err != nil {
+		log.Fatal(err)
+	}
+
+	router := chi.NewMux()
+
+	router.Get("/", handler.MakeHandler(handler.HandleHomeIndex))
+
+	port := os.Getenv("HTTP_LISTEN_ADDR")
+	slog.Info("application running", "port", port)
+	log.Fatal(http.ListenAndServe(port, router))
+}
+
+func initEverything() error {
+	return godotenv.Load()
+}
